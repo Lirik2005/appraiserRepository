c69af35c76093d875c252efb5cf84bdc3359640e
Kiril Gusakov <gusakov24@gmail.com>
2021-09-14 16:59:19 +0300

    AuthUtil imported

--- src/main/java/com/example/appraiserbase/controller/NavigationController.java
+++ src/main/java/com/example/appraiserbase/controller/NavigationController.java
@@ -3,14 +3,19 @@ package com.example.appraiserbase.controller;
 import com.example.appraiserbase.model.Appraiser;
 import com.example.appraiserbase.service.appraiser.AppraiserService;
 import com.example.appraiserbase.service.appraiser.AppraiserServiceImpl;
+import com.example.appraiserbase.utils.AuthUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -26,6 +31,7 @@ public class NavigationController {
     @RequestMapping(value = {"/", "/appraisers"}, method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ADMIN')")
     public String appraisers(Model model) {
+        User user = (User) AuthUtil.getUserFromContext();
         List<Appraiser> appraiserList = appraiserService.getAllAppraisers();
         model.addAttribute("appraisers", appraiserList);
         return "appraisers/appraiser";
